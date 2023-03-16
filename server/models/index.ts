343dcba479b5e6f8c53045ccb99b9c8ab34a77f9
LoneRifle <LoneRifle@users.noreply.github.com>
2023-03-16 13:55:36 +0800

    feat: add extra config if `DATABASE_HOST === pg.neon.tech`
    If we realise we are connecting to Neon's passwordless postgres
    gateway, listen for notice messages from postgres (actually Neon)
    and log these notices to console, so that the user can open the
    given web URL in their browser.
    
    If database host is `pg.neon.tech`,
    - Add notice listener using a Client subclass, inject into
      Sequelize's connection manager
    - Limit connection pool to one maximum, so that the user only
      facilitates a connection just once

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from "sequelize-typescript";
+import { Sequelize, SequelizeOptions } from "sequelize-typescript";
 
 import { User } from "./definitions/User";
 import { Post } from "./definitions/Post";
@@ -7,16 +7,33 @@ import { PostTagAssociation } from "./definitions/PostTagAssociation";
 import { Category } from "./definitions/Category";
 import config from "../config";
 
-const sequelize = new Sequelize({
+import { NoticeLoggingClient } from "./clients/NoticeLoggingClient";
+
+
+const isNeonPasswordless = config.DATABASE_HOST === 'pg.neon.tech'
+
+const sequelizeConfig: SequelizeOptions = {
   host: config.DATABASE_HOST,
   database: config.DATABASE_NAME,
   dialect: "postgres",
   dialectOptions: {
-    ssl: process.env.NODE_ENV === 'production' || config.DATABASE_HOST === 'pg.neon.tech',
+    ssl: process.env.NODE_ENV === 'production' || isNeonPasswordless,
   },
   username: config.DATABASE_USER,
   password: config.DATABASE_PASSWORD,
-});
+}
+if (isNeonPasswordless) {
+  sequelizeConfig.pool = {
+    max: 1,
+  }
+}
+
+const sequelize = new Sequelize(sequelizeConfig);
+
+if (isNeonPasswordless) {
+  // @ts-ignore
+  sequelize.connectionManager.lib.Client = NoticeLoggingClient
+}
 
 sequelize.addModels([User, Category, Post, Tag, PostTagAssociation]);
 
